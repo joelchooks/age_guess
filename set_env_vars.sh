dcac61f1b933da00bc1ab5d4cd1587ad4ce00cb3
joelchooks <joelchukks@gmail.com>
2024-04-24 16:08:19 +0100

    fix deploy

--- set_env_vars.sh
+++ set_env_vars.sh
@@ -13,13 +13,16 @@ if [[ -z "$mr_url" ]]; then
   exit 1
 fi
 
+# Strip "https://" and trailing "/"
+mr_url=$(echo "$mr_url" | sed 's#^https://##' | sed 's#/$##')
+
 echo >> .env.sample
 source .env.sample
 
 while IFS='=' read -r name value; do
   if [[ ! -z "$name" && ! -z "$value" ]]; then
     if [[ "$name" == "ALLOWED_HOSTS" ]]; then
-      value="$value,${mr_url}"
+      value="$value,.${mr_url}"
     fi
     heroku config:set "$name"="$value" --app "$app_name"
   fi
